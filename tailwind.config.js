208e35e8667e30288db6571a1d89307a5e983f36
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-06-27 10:51:47 +0300

    adding tailwind and daisy ui

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import daisyui from "daisyui";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+  themes: ["light", "dark", "cupcake"],
+},
+
+};
