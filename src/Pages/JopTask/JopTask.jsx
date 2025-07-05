dcaa5d20511213b3bc6bad3efc8c85b9103273e8
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-07-05 15:43:46 +0300

    Create JopTask.jsx

---
+++ src/Pages/JopTask/JopTask.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function JopTask() {
+    return (
+        <div className="container px-4 mx-auto w-sm-full">
+            {/* Header */}
+            <div className="p-6 bg-gray-50 shadow-md">
+                <div className="bg-white p-4 shadow-sm">
+                    <h2 className="text-xl font-semibold text-gray-800">Frontend Development</h2>
+                </div>
+
+                <div className="flex flex-wrap gap-6 mt-6 border-b border-black-300 space-x-10">
+                    <button className="pb-2 font-semibold">Details</button>
+                    <button className="pb-2 font-semibold">Quiz</button>
+                    <button className="pb-2 border-b-4 border-black  font-semibold">Task</button>
+                </div>
+            </div>
+            {/**Jop Task */}
+            <h2 className='text-xl font-bold mx-2 my-6'>Technical Task</h2>
+            <div className="bg-white p-6 rounded shadow-sm m-5">
+
+                <div className='flex justify-between items-center'>
+                    <span className="text-xl font-semibold text-gray-900 mb-6">Build a React Todo App</span>
+                    <span className="inline-flex mb-3 items-center gap-1 text-[#166534] text-[13px] bg-[#DCFCE7] rounded-2xl px-3 py-1">submitted</span>
+                </div>
+                <span className='font-normal my-5 mb-6'>Create a simple todo application using React with the ability to add, edit, and delete tasks.</span>
+                <h3 className="text-md my-1 font-semibold text-gray-800 mb-2">Requirements:</h3>
+                <ul className="list-disc list-inside text-gray-700 space-y-2 pl-4">
+
+                    <li>Use React hooks for state management</li>
+                    <li>Implement CRUD operations for todos</li>
+                    <li>Style the application using CSS or a UI library</li>
+                    <li>Include form validation</li>
+                    <li>Implement local storage to persist todos</li>
+                </ul>
+                <div className='flex justify-between items-center my-2'>
+                    <span className='text-[#6B7280] font-light text-[11px]'>Deadline:7/3/2025</span>
+                    <span className=''> <button className='btn bg-[#16A34A] text-white rounded-lg px-8 py-2'>View Details</button></span>
+                </div>
+            </div>
+        </div>
+    )
+}
