661465a9a50376256b3d0468e7714466e4f7bc60
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-07-06 10:46:25 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,4 @@
-// ===== src/App.jsx =====
-import React from "react";
-<<<<<<< HEAD
-import "./App.css";
-import LandingPage from "./Pages/landingpage/LandingPage";
-import HomePage from './Pages/HomePage/HomePage';
-import Track from "./Pages/Track/Track";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Jops from "./Pages/Jops/Jops";
-import JopDetails from "./Pages/JopDetails/JopDetails";
-
-function App() {
-  return (
-    <div>
-      <JopDetails />
-      
-=======
 import ExamPage from "./Pages/student/ExamPage";
 import "./app.css"; 
 
@@ -23,7 +6,6 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <ExamPage />
->>>>>>> 1f04eaa870a786c102f0f52e2477ae9e86d861d0
     </div>
   );
 }
