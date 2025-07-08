1b5a285e180ca5ea62db862f59386b3acbf91feb
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-07-08 23:33:01 +0300

    routes

---
+++ src/Pages/CompanyProfile/CompanyProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CompanyProfile() {
+  return (
+    <div>
+    <div className="header">
+        <div className="img"></div>
+    </div>
+    
+
+    </div>
+  )
+}
